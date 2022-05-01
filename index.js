81e2213d97a3b79a90aaafe7cb2276c364508754
faisalkn-71 <csust2021@gmail.com>
2022-05-01 22:18:49 +0600

    products-api-created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -9,6 +10,38 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = "mongodb+srv://electroStore:UBO6x1OZbQSOFaFD@cluster0.7eqna.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        await client.connect();
+        const productsCollection = client.db('electroStore').collection('products');
+
+
+        //GET Products
+        app.get('/products', async(req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+
+    }
+    finally {
+
+    }
+
+
+}
+
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
