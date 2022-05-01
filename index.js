ded337f942deff3d85cc10096acf4312403704a3
faisalkn-71 <csust2021@gmail.com>
2022-05-01 21:31:15 +0600

    server-commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 
+
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -15,6 +16,7 @@ app.get('/', (req, res) => {
 });
 
 
+// this is the listening part
 app.listen(port, () => {
     console.log('Listening to port', port);
 })
